9b9e4d702bece3b2c4f3896fd62deaf9dc292c97
Isadora Souza <isadora_msouza@hotmail.com>
2019-09-14 14:54:05 -0300

    Inicializando projeto

---
+++ src/main/java/com/isadora/ControleDeTarefas/ControleDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.isadora.ControleDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeTarefasApplication.class, args);
+	}
+
+}
